ef1aa17bd5ea2abeff4db4d7eee028b4c44331ec
Al Chan <alchan5069@gmail.com>
2023-02-02 16:57:16 -0700

    Initial setup for Hotel Listing, with migrations for country and hotel models.

---
+++ MyHotelListingAPI/Data/HotelListingDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyHotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options) 
+        {
+               
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id= 1,
+                    Name = "Japan",
+                    ShortName = "JP"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Canada",
+                    ShortName = "CA"
+                }
+             );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Napon",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Canada Resort and Spa",
+                    Address = "Calgary",
+                    CountryId = 2,
+                    Rating = 5.0
+                }
+             );
+        }
+    }
+}
---
+++ MyHotelListingAPI/Migrations/20230202235452_SeedCountryAndHotels.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MyHotelListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCountryAndHotels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "Id", "Name", "ShortName" },
+                values: new object[,]
+                {
+                    { 1, "Japan", "JP" },
+                    { 2, "Canada", "CA" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotels",
+                columns: new[] { "Id", "Address", "CountryId", "Name", "Rating" },
+                values: new object[,]
+                {
+                    { 1, "Napon", 1, "Sandals Resort and Spa", 4.5 },
+                    { 2, "Calgary", 2, "Canada Resort and Spa", 5.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
