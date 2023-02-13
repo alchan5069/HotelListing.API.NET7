2153fd8b74a169a72056b65c80747e3c8e55f919
Al Chan <alchan5069@gmail.com>
2023-02-13 15:38:10 -0700

    Add scaffolding DB context factory

--- HotelListingAPI.Data/HotelListingDbContext.cs
+++ HotelListingAPI.Data/HotelListingDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using MyHotelListingAPI.Data.Configurations;
 
 namespace MyHotelListingAPI.Data
@@ -15,10 +17,26 @@ namespace MyHotelListingAPI.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            
+
             modelBuilder.ApplyConfiguration(new RoleConfiguration());
             modelBuilder.ApplyConfiguration(new CountryConfiguration());
             modelBuilder.ApplyConfiguration(new HotelConfiguration());
+        }
+    }
+
+    public class HotelListingDbContextFactory : IDesignTimeDbContextFactory<HotelListingDbContext>
+    {
+        public HotelListingDbContext CreateDbContext(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<HotelListingDbContext>();
+            var conn = config.GetConnectionString("HotelListingDbConnectionString");
+            optionsBuilder.UseSqlServer(conn);
+            return new HotelListingDbContext(optionsBuilder.Options);
         }
     }
 }
